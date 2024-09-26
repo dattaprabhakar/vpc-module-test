4a22d31121165f2211cd139604a96efbde8a0ef5
prabhakararao.nandigrama <prabhakararao.nandigrama@assettl.com>
2024-09-26 22:36:25 +0530

    updated

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "prabha-state-dev"
+    key    = "expense-vpc"
+    region = "us-east-1"
+    dynamodb_table = "lock-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
